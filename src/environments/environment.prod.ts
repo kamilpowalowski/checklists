b8a5f77b599161273395f1bf0d3a9438c1fdad60
Kamil Powałowski <kamil.powalowski@gmail.com>
2018-01-05 09:55:39 +0100

    Add Firebase configuration to production enviroment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAOOjobvFzapuiSAqaPB7J-jEvvlZmoQTA',
+    authDomain: 'checklists-75c83.firebaseapp.com',
+    databaseURL: 'https://checklists-75c83.firebaseio.com',
+    projectId: 'checklists-75c83',
+    storageBucket: 'checklists-75c83.appspot.com',
+    messagingSenderId: '513542089540'
+  }
 };

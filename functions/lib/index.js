54e7a626412c927f065ae9e7b52ad6749c44353f
Kamil Powałowski <kamil.powalowski@gmail.com>
2018-04-01 16:47:32 +0200

    Removed prerender.io and added own prerendering algorithm

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,18 +1,73 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const path = require("path");
 const prerenderNode = require("prerender-node");
-const app = express();
-if (functions.config().prerenderio && functions.config().prerenderio.token) {
-    const token = functions.config().prerenderio.token;
-    app.use(prerenderNode.set('prerenderToken', token));
+admin.initializeApp(functions.config().firebase);
+function buildHtmlWithChecklist(checklist, url) {
+    const title = checklist.title + ' - lizt.co - checklists made easy';
+    const string = '<!doctype html><html lang="en">' +
+        '<meta charset="utf-8">' +
+        '<title>' + title + '</title>' +
+        '<meta property="og:title" content="' + title + '">' +
+        '<meta property="twitter:title" content="' + title + '">' +
+        '<meta property="description" content="' + checklist.description + '">' +
+        '<meta property="og:description" content="' + checklist.description + '">' +
+        '<meta property="twitter:description" content="' + checklist.description + '">' +
+        '<meta property="og:url" content="' + url + '">' +
+        '<meta property="twitter:url" content="' + url + '">' +
+        '<meta property="og:type" content="website">' +
+        '<meta property="og:locale" content="en_US">' +
+        '<link rel="icon" type="image/x-icon" href="https://lizt.co/favicon.ico">' +
+        '</head></html>';
+    return string;
 }
-function angularResponse(request, response) {
-    response.sendFile(path.join(__dirname + '/../dist/index.html'));
+function buildDefaultHtml(url) {
+    const title = 'lizt.co - checklists made easy';
+    const description = 'community driven website for creating and sharing checklists';
+    const string = '<!doctype html><html lang="en">' +
+        '<meta charset="utf-8">' +
+        '<title>' + title + '</title>' +
+        '<meta property="og:title" content="' + title + '">' +
+        '<meta property="twitter:title" content="' + title + '">' +
+        '<meta property="description" content="' + description + '">' +
+        '<meta property="og:description" content="' + description + '">' +
+        '<meta property="twitter:description" content="' + description + '">' +
+        '<meta property="og:url" content="' + url + '">' +
+        '<meta property="twitter:url" content="' + url + '">' +
+        '<meta property="og:type" content="website">' +
+        '<meta property="og:locale" content="en_US">' +
+        '<link rel="icon" type="image/x-icon" href="https://lizt.co/favicon.ico">' +
+        '</head></html>';
+    return string;
 }
-app.use(express.static(`${__dirname}/../dist`));
-app.get('*', angularResponse);
-exports.application = functions.https.onRequest(app);
+function returnDefaultHtml(request, response) {
+    const html = buildDefaultHtml(request.path);
+    response.status(200).end(html);
+}
+exports.application = functions.https.onRequest((request, response) => {
+    console.log(prerenderNode);
+    if (!prerenderNode.shouldShowPrerenderedPage(request)) {
+        response.sendFile(path.join(__dirname + '/../dist/index.html'));
+        return;
+    }
+    const pathElements = request.path.split('/');
+    if (pathElements[1] === 'checklists' && pathElements[2]) {
+        admin.firestore()
+            .collection('checklists')
+            .doc(pathElements[2])
+            .get()
+            .then((snapshot) => {
+            const checklist = snapshot.data();
+            const checklistHtml = buildHtmlWithChecklist(checklist, request.path);
+            response.status(200).end(checklistHtml);
+        })
+            .catch((reason) => {
+            returnDefaultHtml(request, response);
+        });
+        return;
+    }
+    returnDefaultHtml(request, response);
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,79 @@
-import * as express from 'express';
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as path from 'path';
 import * as prerenderNode from 'prerender-node';
 
-const app = express();
+admin.initializeApp(functions.config().firebase);
 
-if (functions.config().prerenderio && functions.config().prerenderio.token) {
-  const token = functions.config().prerenderio.token;
-  app.use(prerenderNode.set('prerenderToken', token));
+function buildHtmlWithChecklist(checklist: any, url: string) {
+  const title = checklist.title + ' - lizt.co - checklists made easy';
+  const string = '<!doctype html><html lang="en">' +
+    '<meta charset="utf-8">' +
+    '<title>' + title + '</title>' +
+    '<meta property="og:title" content="' + title + '">' +
+    '<meta property="twitter:title" content="' + title + '">' +
+    '<meta property="description" content="' + checklist.description + '">' +
+    '<meta property="og:description" content="' + checklist.description + '">' +
+    '<meta property="twitter:description" content="' + checklist.description + '">' +
+    '<meta property="og:url" content="' + url + '">' +
+    '<meta property="twitter:url" content="' + url + '">' +
+    '<meta property="og:type" content="website">' +
+    '<meta property="og:locale" content="en_US">' +
+    '<link rel="icon" type="image/x-icon" href="https://lizt.co/favicon.ico">' +
+    '</head></html>';
+  return string;
 }
 
-function angularResponse(request, response) {
-  response.sendFile(path.join(__dirname + '/../dist/index.html'));
+function buildDefaultHtml(url: string) {
+  const title = 'lizt.co - checklists made easy';
+  const description = 'community driven website for creating and sharing checklists';
+  const string = '<!doctype html><html lang="en">' +
+    '<meta charset="utf-8">' +
+    '<title>' + title + '</title>' +
+    '<meta property="og:title" content="' + title + '">' +
+    '<meta property="twitter:title" content="' + title + '">' +
+    '<meta property="description" content="' + description + '">' +
+    '<meta property="og:description" content="' + description + '">' +
+    '<meta property="twitter:description" content="' + description + '">' +
+    '<meta property="og:url" content="' + url + '">' +
+    '<meta property="twitter:url" content="' + url + '">' +
+    '<meta property="og:type" content="website">' +
+    '<meta property="og:locale" content="en_US">' +
+    '<link rel="icon" type="image/x-icon" href="https://lizt.co/favicon.ico">' +
+    '</head></html>';
+  return string;
 }
 
-app.use(express.static(`${__dirname}/../dist`));
-app.get('*', angularResponse);
+function returnDefaultHtml(request, response) {
+  const html = buildDefaultHtml(request.path);
+  response.status(200).end(html);
+}
 
-export const application = functions.https.onRequest(app);
+
+export const application = functions.https.onRequest((request, response) => {
+  console.log(prerenderNode);
+  if (!prerenderNode.shouldShowPrerenderedPage(request)) {
+    response.sendFile(path.join(__dirname + '/../dist/index.html'));
+    return;
+  }
+
+  const pathElements = request.path.split('/');
+  if (pathElements[1] === 'checklists' && pathElements[2]) {
+    admin.firestore()
+      .collection('checklists')
+      .doc(pathElements[2])
+      .get()
+      .then((snapshot) => {
+        const checklist = snapshot.data();
+        const checklistHtml = buildHtmlWithChecklist(checklist, request.path);
+        response.status(200).end(checklistHtml);
+      })
+      .catch((reason) => {
+        returnDefaultHtml(request, response);
+      });
+    return;
+  }
+
+  returnDefaultHtml(request, response);
+
+});

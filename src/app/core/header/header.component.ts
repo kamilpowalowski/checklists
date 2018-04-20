dc642680239413ae60589f213048f70cb0f58f63
Kamil Powałowski <kamil.powalowski@gmail.com>
2018-04-21 00:20:08 +0200

    Changed access rules for checklist subitems.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -61,7 +61,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.userName = user.displayName ? user.displayName : 'Active user';
     this.userPhoto = user.photo;
     this.userMenu = [
-      { title: 'My account', link: '/account' },
+      // { title: 'My account', link: '/account' },
       { title: 'Log out', link: '/auth/logout' }
     ];
   }
